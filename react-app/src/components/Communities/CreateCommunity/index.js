252a8a34e12d116b190d73301b7628422a6e782b
alex dam <alexdam55@gmail.com>
2022-11-04 17:12:36 -0700

    create community almost fully styled

--- react-app/src/components/Communities/CreateCommunity/index.js
+++ react-app/src/components/Communities/CreateCommunity/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { createCommunityThunk } from "../../../store/community";
+import icon from "../../../assets/icon.png"
 import "./CreateCommunity.css"
 
 
@@ -67,17 +68,23 @@ const CreateCommunity = () => {
                                 {showErrors &&
                                     <ul className="errors">
                                         {validationErrors.map((e, i) => {
-                                            return <div className='create-spot-error-message' key={i}>{e}</ div>
+                                            return <div className='create-community-error-message' key={i}>{e}</ div>
                                         })}
                                     </ul>
                                 }
                                 <div className="community-form-content">
                                     <div className="create-community-name-wrapper">
                                         <div className="create-community-name-container">
+                                            <div className="create-community-name-text">
+                                                <div className="create-community-name">
+                                                    Community Name:
+                                                </div>
+                                                <div className="create-community-disclaimer">Community names cannot be changed or edited.</div>
+                                            </div>
                                             <input
                                                 className="create-community-name-body"
                                                 type="text"
-                                                placeholder="name"
+                                                placeholder="Circus"
                                                 value={name}
                                                 onChange={updateName}
                                                 required />
@@ -85,17 +92,22 @@ const CreateCommunity = () => {
                                     </div>
                                     <div className="create-community-about-wrapper">
                                         <div className="create-community-about-container">
+                                            <div className="create-community-name-text">Tell us about your community:</div>
                                             <textarea
                                                 className="create-community-about-body"
                                                 type="text"
-                                                placeholder="about"
+                                                placeholder="The Circus is a community made by programmers, for programmers. The goal is to answer any coding related questions with the hopes that all members of the Circus will find sucessful jobs in the tech industry."
                                                 value={about}
                                                 onChange={updateAbout}
                                                 required />
+                                            <div className="create-form-description-count">
+                                                {about.length} / 200
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="create-community-banner-wrapper">
                                         <div className="create-community-banner-container">
+                                            <div className="create-community-name-text">Community Banner Image (Optional): </div>
                                             <input
                                                 className="create-community-banner-body"
                                                 type="text"
@@ -106,6 +118,7 @@ const CreateCommunity = () => {
                                     </div>
                                     <div className="create-community-icon-wrapper">
                                         <div className="create-community-icon-container">
+                                        <div className="create-community-name-text">Community Icon Image:</div>
                                             <input
                                                 className="create-community-icon-body"
                                                 type="text"
@@ -116,17 +129,45 @@ const CreateCommunity = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <button
-                                    type='submit'>
-                                    Create your Community
-                                </button>
-                                <button
-                                    type='button'
-                                    onClick={handleCancel}>
-                                    Cancel
-                                </button>
+                                <div className="create-community-bottom-section">
+                                    <div className="create-form-borders"></div>
+                                    <div className="create-community-buttons-container">
+                                        <div className="create-community-buttons">
+                                            <div className="create-post-cancel-container">
+                                                <button
+                                                    className="create-post-cancel"
+                                                    type='button'
+                                                    onClick={handleCancel}>
+                                                    Cancel
+                                                </button>
+                                            </div>
+                                            <div className="create-community-confirm">
+                                                <button
+                                                    className="create-community-button"
+                                                    type='submit'>
+                                                    Create
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </form>
                         </div>
+                    </div>
+                </div>
+                <div className="create-post-right">
+                    <div className="create-post-rules-container">
+                        <div className="create-post-rules-header">
+                            <img src={icon} alt='icon' className="rules-icon" />
+                            Posting to Sawdat
+                        </div>
+                        <ol className="create-post-rules">
+                            <li className="create-post-individual-rule">Remember the users</li>
+                            <li className="create-post-individual-rule">Treat others how you want to be treated</li>
+                            <li className="create-post-individual-rule">Don't steal other people's code</li>
+                            <li className="create-post-individual-rule">Remove default styling</li>
+                            <li className="create-post-individual-rule">Chase progress, not perfection</li>
+                        </ol>
                     </div>
                 </div>
             </div>
