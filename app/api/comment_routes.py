d2a9d4f3c7e0941a316f598dca350a68e38886c7
alex dam <alexdam55@gmail.com>
2023-06-22 14:17:46 -0700

    added delete to comments

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_login import current_user, login_required
-from app.models import Comment, Post
+from app.models import Comment, Post, User, db
 from app.forms.comment_form import CommentForm
 
 
@@ -20,3 +20,20 @@ comment_routes = Blueprint("comments", __name__)
 def get_all_comments():
     comments = Comment.query.all()
 
+# DELETE A COMMENT
+
+
+@comment_routes.route("/<int:id", methods=["DELETE"])
+@login_required
+def delete_comment(id):
+    comment = Comment.query.get(id)
+    if not comment:
+        return {"message": "Comment couldn't be found", "statusCode": 404}
+
+    if not comment.user_id == current_user.id:
+        return {"message": "Forbidden", "statusCode": 403}
+
+    db.session.delete(comment)
+    db.session.commit()
+
+    return {"message": "Successfully delete", "statusCode": 200}
